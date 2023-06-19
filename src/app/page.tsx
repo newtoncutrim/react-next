b9426afcd16946d4446297e861addb00e884027e
newtoncutrim <newtoncutrim777@gmail.com>
2023-06-19 14:50:09 -0300

    importaçao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from '@/components/Segundo';
+import {Teste, Segundo} from '@/components/Teste';
+
 
 const Page = () => {
   return (
     <div className="font-bold">
         <h1>o bagulho e doido</h1>
+        <Teste/>
+        <Button/>
+        <Segundo/>
     </div>
   );
 }
---
+++ src/components/Teste.tsx
@@ -0,0 +1,13 @@
+export const Teste = () => {
+    return (
+        <div className="w-40 h-40 bg-blue-600">
+            <h1>frase qualquer</h1>
+        </div>
+    );
+}
+
+export const Segundo = () => {
+    return(
+        <h1>janela</h1>
+    );
+}
