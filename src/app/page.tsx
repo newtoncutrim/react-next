7ea4a21c947aed74144c43e0e2a4b75473f9971c
newtoncutrim <newtoncutrim777@gmail.com>
2023-06-22 11:38:48 -0300

    teste2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,17 @@ const getDay = ()  => {
   return new Intl.DateTimeFormat('pt-BR', {weekday: 'long'}).format(new Date());
 }
 const Page = () => {
-  const nome: string = 'newton';
-  const foguete: string = "https://img.olhardigital.com.br/wp-content/uploads/2023/01/Destaque-foguete-1024x576.jpg";
+  const data = {
+    nome: 'newton',
+    foguete: "https://img.olhardigital.com.br/wp-content/uploads/2023/01/Destaque-foguete-1024x576.jpg"
+  }
+  
   return (
     <div className="font-bold">
-        <h1>o bagulho e doido {nome} - {getDay()}</h1>
+        <h1>o bagulho e doido {data.nome} - {getDay()}</h1>
         <Content />
-        <img src={foguete} />
-        <span>{40 + 23}</span>
+        <img src={data.foguete} />
+        <span>{40 + 23}</span> 
         <h1 style={{color: 'red'}}> opa</h1>
     </div>
   );
