f273244a6c7e7449c4cc6cf3a5f35281502e2909
newtoncutrim <newtoncutrim777@gmail.com>
2023-06-27 21:37:16 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { Interno } from '@/components/interno';
   return new Intl.DateTimeFormat('pt-BR', { weekday: 'long' }).format(new Date());
 }; */
 
-const getFrase = (hora: number): string => {
+/* const getFrase = (hora: number): string => {
   if (hora >= 6 && hora < 12) {
     return 'Bom dia!';
   } else if (hora >= 12 && hora < 18) {
@@ -17,18 +17,20 @@ const getFrase = (hora: number): string => {
   } else {
     return 'Boa noite!';
   }
-};
+}; */
 
 const Page = () => {
   const agora = new Date();
   const hora = agora.getHours();
   const minutos = agora.getMinutes();
-  const frase = getFrase(hora);
+  /* const frase = getFrase(hora); */
 
   return (
     <div className="flex items-center flex-col justify-center h-screen bg-gradient-to-r from-blue-500 to-blue-700">
       <div className="text-9xl text-white block">{hora}:{minutos}</div>
-      <div className="text-4xl text-white block">{frase}</div>
+     <div className="text-4xl text-white block">{hora > 0 && hora < 11 && 'BOM DIA'} {hora >= 11 && hora < 16 && 'BOA TARDE'} {hora >= 16 && hora < 12 && 'BOA NOITE'}
+     
+     </div>
     </div>
   );
 };
